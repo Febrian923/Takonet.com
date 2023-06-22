9a7a7ae84a328db71a30102379e859868ea7f0b2
Febrian923 <135141733+Febrian923@users.noreply.github.com>
2023-06-22 11:07:21 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,25 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Ambil data yang dikirim melalui form
-    $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $confirmPassword = $_POST['confirm_password'];
 
-    // Validasi data (misalnya: cek apakah password dan konfirmasi password sama)
-    if ($password !== $confirmPassword) {
-        echo 'Konfirmasi password tidak sesuai';
-        exit;
+    // Proses validasi data login
+    // Misalnya, cek apakah email dan password sesuai dengan data yang ada dalam database
+
+    // Contoh: Membaca data dari file CSV
+    $file = fopen('data.csv', 'r');
+    while (($row = fgetcsv($file)) !== false) {
+        $savedEmail = $row[1];
+        $savedPassword = $row[2];
+        if ($savedEmail === $email && $savedPassword === $password) {
+            echo 'Login berhasil!';
+            fclose($file);
+            exit;
+        }
     }
-
-    // Proses penyimpanan data ke database atau langkah-langkah pendaftaran lainnya
-
-    // Contoh: Menyimpan data dalam file CSV
-    $data = [
-        $name,
-        $email,
-        $password
-    ];
-    $file = fopen('data.csv', 'a');
-    fputcsv($file, $data);
     fclose($file);
 
-    echo 'Pendaftaran berhasil!';
+    echo 'Email atau password salah.';
 }
 ?>
